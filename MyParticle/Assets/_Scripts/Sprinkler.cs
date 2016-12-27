d285ad303c0620afff4efb2e879e9b2e2ae50a25
木子 <819241959@qq.com>
2016-12-27 17:21:08 +0800

    添加冒烟和下雨的粒子特效

---
+++ MyParticle/Assets/_Scripts/Sprinkler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sprinkler : MonoBehaviour {
+    private float heightAboveFloor;
+    private ParticleSystem[] sprinklers;
+    void Awake()
+    {
+        sprinklers = GetComponentsInChildren<ParticleSystem>();
+    }
+	// Use this for initialization
+	void Start () {
+        heightAboveFloor = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(ray);
+            foreach (RaycastHit h in hits)
+            {
+                if (h.collider.name == "ground")
+                    transform.position = h.point + new Vector3(0f, heightAboveFloor, 0f);
+            }
+            if(!sprinklers[0].isPlaying)
+            {
+                for(int i=0;i<sprinklers.Length;i++)
+                {
+                    sprinklers[i].Play();
+                }
+
+            }
+        }
+        else
+        {
+            if(sprinklers[0].isPlaying)
+            {
+                for(int i=0;i<sprinklers.Length;i++)
+                {
+                    sprinklers[i].Stop();
+                }
+            }
+        }
+	}
+}
